c046f85a96e8281103718c903e05bf5bf6adfea6
Pedro <157895515+goncalvespedrogom@users.noreply.github.com>
2024-10-31 11:09:38 -0300

    Alterando CSS

--- form-multi-step-react/src/App.jsx
+++ form-multi-step-react/src/App.jsx
@@ -6,6 +6,10 @@ import ReviewForm from "./components/ReviewForm";
 import Thanks from "./components/Thanks";
 import Steps from "./components/Steps";
 
+import { BsArrowRight } from "react-icons/bs";
+import { BsArrowLeft } from "react-icons/bs";
+import { BsFillSendFill } from "react-icons/bs";
+
 // hooks
 import { useForm } from "./hooks/useForm";
 
@@ -39,33 +43,34 @@ function App() {
 
   return (
     <div className="app">
-      <div className="header">
-        <h2>Deixe sua avaliação</h2>
-        <p>
-          Ficamos felizes com a sua compra, utilize o formulário abaixo para
-          avaliar o produto
-        </p>
-      </div>
+      
       <div className="form-container">
+        <div className="header">
+          <h2>Deixe sua avaliação</h2>
+          <p>
+            Esperamos que você tenha gostado da sua compra, preencha o formulário abaixo para
+            avaliar o nosso produto
+          </p>
+        </div>
         <Steps currentStep={currentStep} />
         <form onSubmit={(e) => changeStep(currentStep + 1, e)}>
           <div className="inputs-container">{currentComponent}</div>
           <div className="actions">
             {!isFirstStep && (
               <button type="button" onClick={() => changeStep(currentStep - 1)}>
-                <GrFormPrevious />
+                <BsArrowLeft />
                 <span>Voltar</span>
-              </button>
+              </button> 
             )}
             {!isLastStep ? (
               <button type="submit">
                 <span>Avançar</span>
-                <GrFormNext />
+                <BsArrowRight />
               </button>
             ) : (
               <button type="button">
                 <span>Enviar</span>
-                <FiSend />
+                <BsFillSendFill />
               </button>
             )}
           </div>
--- form-multi-step-react/src/components/Steps.jsx
+++ form-multi-step-react/src/components/Steps.jsx
@@ -1,21 +1,25 @@
 import {AiOutlineUser, AiOutlineStar} from "react-icons/ai";
 import { FiSend } from "react-icons/fi";
 
+import { BsSend } from "react-icons/bs";
+import { BsStar } from "react-icons/bs";
+import { BsPerson } from "react-icons/bs";
+
 import "./Steps.css";
 
 const Steps = ({ currentStep }) => {
   return (
     <div className="steps">
       <div className="step active">
-        <AiOutlineUser />
+        <BsPerson />
         <p>Identificação</p>
       </div>
       <div className={`step ${currentStep >= 1 ? "active" : ""}`}>
-        <AiOutlineStar />
+        <BsStar />
         <p>Avaliação</p>
       </div>
       <div className={`step ${currentStep >= 2 ? "active" : ""}`}>
-        <FiSend />
+        <BsSend />
         <p>Envio</p>
       </div>
     </div>
